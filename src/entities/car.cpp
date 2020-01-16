9cc784d409fd976dd03c2727e30ae7a79f1149f1
Yureehh <fabbri.juri@gmail.com>
2020-01-16 20:09:59 +0100

    Adjustment
    Expanded the layout despite the score sectiion

---
+++ src/entities/car.cpp
@@ -0,0 +1,48 @@
+#include "car.h"
+
+//12 13 14
+// O O
+//  O
+// O O
+
+car::car(int asx = 12, int asy = 27,
+        int adx = 14, int ady = 27,
+        int bsx = 12, int bsy = 29,
+        int bdx = 14, int bdy = 29){
+
+        as.x = asx; as.y = asy;
+        ad.x = adx; ad.y = ady;
+        bs.x = bsx; bs.y = bsy;
+        bd.x = bdx; bd.y = bdy;
+}
+    
+bool car::destra(){
+    if(ad.x < 26){
+        as.x++;
+        ad.x++;
+        bs.x++;
+        bd.x++;
+        return true;
+    }
+    else{
+        return false;
+    } 
+}
+
+bool car::sinistra(){
+    if(as.x >2){
+        as.x--;
+        ad.x--;
+        bs.x--;
+        bd.x--;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+
+
+
+
