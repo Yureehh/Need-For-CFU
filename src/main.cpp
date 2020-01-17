331bee233f7ab8ae4584b27bc7e313dd9c49faf1
Filippo Bertozzi <filippo.bertozzi00@gmail.com>
2020-01-17 12:30:51 +0100

    Figa la macchina si muove
    deh

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 #include <unistd.h>
 #include "layout.h"
-#include "car.h"
+#include "entities\car.h"
 
 int main(int argc, char *argv[]){
 
@@ -13,31 +13,38 @@ int main(int argc, char *argv[]){
     curs_set(FALSE);
     
     //Starting the game
-    mvprintw(0, 0, "premi un tasto per iniziare a giocare");
+    mvprintw(21, 11, "Press Any Key To Start!");
     getch();
     erase();
-    mvprintw(0, 0, "Il gioco inizia fra 3");
-    refresh();
-    sleep(1);
-    mvprintw(0, 0, "Il gioco inizia fra 2");
-    refresh();
-    sleep(1);
-    mvprintw(0, 0, "Il gioco inizia fra 1");
-    refresh();
-    sleep(1);
-
     //Stamp the UI and the car
     layout l = layout();
-    l.stampa_UI();   
-    car c = car(38,23,40,23,39,24,38,25,40,25);
+    l.stampa_UI();
+    mvprintw(21, 24, "3");
+    refresh();
+    sleep(1);
+    mvprintw(21, 24, "2");
+    refresh();
+    sleep(1);
+    mvprintw(21, 24, "1");
+    refresh();
+    sleep(1);
+    mvprintw(21, 24, " ");
+
+       
+    car c = car(38,23,40,23,39,24,38,25,40,25); //never cancellare pls 
     c.stampa();
     refresh();
-    int ch = getch();
+    int ch ='7';
     while(ch != ' '){
-        if(ch == 'a' || ch == KEY_LEFT)
+        ch= getch();
+        if(ch == 'a' || ch == KEY_LEFT){
+            c.clean();
             c.sinistra();
-        if(ch == 'd' || ch == KEY_RIGHT)
+        }   
+        if(ch == 'd' || ch == KEY_RIGHT){
+            c.clean();
             c.destra();
+        }
         c.stampa();
         refresh();
     }
