8b6702f97c4c759a18f8d1d36dc1dd29fe147137
Yureehh <fabbri.juri@gmail.com>
2020-01-19 11:00:25 +0100

    Border Collisions and game over

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 #include <cstdlib>
 #include <unistd.h>
 #include "layout.h"
-
+#include "scorestage.h"
+#include "entities\car.h"
+/*
 #ifdef __linux__ 
     #include "entities/car.h"
 #elif _WIN32
@@ -11,6 +13,7 @@
 #else
     cout << "Hai un OS sfigato cosa ti posso dire"
 #endif
+*/
 
 int main(int argc, char *argv[]){
 
@@ -18,7 +21,12 @@ int main(int argc, char *argv[]){
     initscr();
     noecho();
     curs_set(FALSE);
-    
+    /*
+    start_color();
+    init_pair(1, COLOR_BLACK, COLOR_RED);
+    init_pair(2, COLOR_WHITE, COLOR_BLACK);
+    */
+
     //Starting the game
     mvprintw(21, 11, "Press Any Key To Start!");
     getch();
@@ -27,6 +35,8 @@ int main(int argc, char *argv[]){
     layout l = layout();
     l.stampaUI();
     l.stampaScore();
+    scorestage s = scorestage(2300);
+    s.PrintScoreStage();
     mvprintw(21, 24, "3");
     refresh();
     sleep(1);
@@ -43,18 +53,33 @@ int main(int argc, char *argv[]){
     c.stampa();
     refresh();
     int ch ='7';
-    while(ch != ' '){
+    bool spostamento = true, loss = false;
+    while(ch != ' ' && !loss){
         ch= getch();
         if(ch == 'a' || ch == 68){
             c.clean();
-            c.sinistra();
+            spostamento = c.sinistra();
         }   
         if(ch == 'd' || ch == 67){
             c.clean();
-            c.destra();
+            spostamento = c.destra();
         }
+        if(!spostamento){
+            spostamento = true;
+            s.SubScore(500);
+            if(s.GetScore() <= 0){
+                loss = true;
+            }
+        }
+        s.PrintScoreStage();
         c.stampa();
         refresh();
+    }
+
+    if(loss){
+        erase();
+        mvprintw(21, 11, "Take the L!");
+        getch();
     }
 
     endwin();
