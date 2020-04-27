81a7f4cc2b40f0fb8954c4d550b8676c26d29269
AlePini <djpini00@gmail.com>
2020-04-27 12:34:46 +0200

    Created the Start Menu, not fancy but practical

--- makefile
+++ makefile
@@ -6,10 +6,10 @@ SRC_ENT = car obstacle
 all : clean outrun
 
 outrun : $(addprefix entities/,$(SRC_ENT)) $(SRC)	
-	$(CC) bin/*.o -o outrun -lncurses
+	$(CC) bin/*.o -o outrun -lncurses -lmenu
 
 %: src/%.cpp
-	$(CC) $(CFLAGS) $< -o bin/$@.o -lncurses
+	$(CC) $(CFLAGS) $< -o bin/$@.o -lncurses -lmenu
 	
 entities/% : src/entities/%.cpp
 	$(CC) $(CFLAGS) $< -o bin/$(subst entities/,,$@).o -lncurses
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <menu.h>
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
@@ -8,24 +9,105 @@
 
 // ! Usare mvinch(x,y) per sapere che char è fatto vedere in x,y
 
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define CTRLD 	4
+
+const char *choices[] = {
+                        "Start Game",
+                        "Quit",
+                  };
+
 int main(int argc, char *argv[]){
+
+    int termX, termY;
+
+    ITEM** my_items;
+	int c;				
+	MENU *my_menu;
+	int n_choices, i;
+	ITEM *cur_item;
 
     // _ Setup ncurses
     initscr();
+
+    // Setup Colors
     start_color();
-
     use_default_colors();
-
     init_pair(1, COLOR_RED, -1);
     init_pair(2, COLOR_GREEN, -1);
 
     noecho();
     curs_set(FALSE);
     nodelay(stdscr, TRUE);
+    keypad(stdscr, TRUE);
 
-    //Starting the game
-    mvprintw(21, 11, "Press Key To Start!");
-    while(!kbhit());
+    getmaxyx(stdscr, termY, termX);
+
+    // _ Menù
+    // ? Set window
+    WINDOW *my_menu_win = newwin(termY, termX, 0, 0);
+
+    
+
+    keypad(my_menu_win, TRUE);
+
+    // ? Print title
+    mvwprintw(my_menu_win, termY/4 - 2, termX/2 - 42, "_____   __         _________   __________                 ______________________  __");
+    mvwprintw(my_menu_win, termY/4 - 1, termX/2 - 42, "___  | / /_______________  /   ___  ____/_____________    __  ____/__  ____/_  / / /");
+    mvwprintw(my_menu_win, termY/4    , termX/2 - 42, "__   |/ /_  _ \\  _ \\  __  /    __  /_   _  __ \\_  ___/    _  /    __  /_   _  / / /");
+    mvwprintw(my_menu_win, termY/4 + 1, termX/2 - 42, "_  /|  / /  __/  __/ /_/ /     _  __/   / /_/ /  /        / /___  _  __/   / /_/ /");
+    mvwprintw(my_menu_win, termY/4 + 2, termX/2 - 42, "/_/ |_/  \\___/\\___/\\__,_/      /_/      \\____//_/         \\____/  /_/      \\____/");
+
+    // ? Set up menu choices
+    n_choices = ARRAY_SIZE(choices);
+	my_items = new ITEM*[n_choices + 1];
+
+	for(i = 0; i < n_choices; ++i)
+	        my_items[i] = new_item(choices[i],"");
+	my_items[n_choices] = (ITEM *)NULL;
+
+	my_menu = new_menu(my_items);
+
+    // ? Menu's Windows
+    set_menu_win(my_menu, my_menu_win);
+    set_menu_sub(my_menu, derwin(my_menu_win, 6, 13, termY/4*3, termX/2 - 6));
+    
+    set_menu_mark(my_menu, " * ");
+
+    // ? Post-it
+	post_menu(my_menu);
+	wrefresh(my_menu_win);
+
+    int startgame = false;
+
+	while(!startgame && (c = wgetch(my_menu_win)) != KEY_F(1)){
+        switch(c){
+            case KEY_DOWN:
+		        menu_driver(my_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(my_menu, REQ_UP_ITEM);
+				break;
+            case 10: // ENTER
+                switch(item_index(current_item(my_menu))){
+                    case 0:
+                        startgame = true;
+                        break;
+                    case 1:
+                        endwin();
+                        exit(0);
+                        break;
+                }
+		}
+        wrefresh(my_menu_win);
+	}
+    
+    // ? Clear all the menu
+    unpost_menu(my_menu);
+    free_menu(my_menu);
+    for(i = 0; i < n_choices; ++i)
+            free_item(my_items[i]);
+
     erase();
 
     //Print the UI
